f4892961e405847510b5887d2b8f1fed1e79d966
Keira Innes <92391778+TheBeezKneez@users.noreply.github.com>
2021-12-01 03:45:00 +0000

    Fixed my "check for correct input" code and finished the game

--- A3GuessNum.java
+++ A3GuessNum.java
@@ -1,10 +1,7 @@
 /*
 *Name: Keira Innes
-*Date: Nov 23, 2021
+*Date: Nov 30, 2021
 *Desription: Number guessing game
-A3GuessNum**************
- hasNextInt() thing
- https://stackoverflow.com/questions/18119211/how-to-check-if-user-input-is-not-an-int-value/18119273
 */
 import java.util.*;
 
@@ -16,39 +13,47 @@ public class A3GuessNum
     static int guess;
     static boolean win = false;
     static int chancesLeft = 5;
-    static boolean numberExeption = false;
     
     
     //Method 1
     public static void mainGame(){
 
-        System.out.println(dieRoll);
-        
-        //If there is an invalid input type
+        //If there is invalid input
         while (true){
+            
+            boolean numberExeption = false;
+            
+            //record the users input
             Scanner input = new Scanner(System.in);
-            System.out.println("Guess a number");
+            System.out.println("Guess a number.\n");
             guessString = input.nextLine();
             
             try{
                 guess = Integer.parseInt(guessString);
             }
-            catch (NumberFormatException ex){
+            catch (NumberFormatException ex){   //if it catches an error
                 numberExeption = true;
-                System.out.println("You've entered an invalid input type, try again.");
-                System.out.println(numberExeption);
+                System.out.println("You've entered an invalid input type, try again.\n");
             }
-            if (numberExeption = false){
+            
+            //If program doesn't catch a number exeption
+            if (!numberExeption){
                 guess = Integer.parseInt(guessString);
-                break;
+                
+                if (guess > 100 || guess < 1){  //if out of bounds
+                    System.out.println("You've entered an out of bounds number. Remember, the secret number is only between 1-100. Try again.\n");
+                }
+                else{   //if input works
+                    break;
+                }
             }
-            System.out.println("The program did not exit the loop >:(");
+            
             
         }//close while loop
     }//close method 
         
     
-    //Method 2 (contains most of the game)
+    //Method 2
     public static void fullGame(){
         while (!win && chancesLeft != -1){
             
@@ -56,7 +61,7 @@ public class A3GuessNum
             
             //If you won
             if (guess == dieRoll){
-                System.out.println("You won!!");
+                System.out.println("\nYou won!!");
                 win = true;
             }
             
@@ -66,7 +71,7 @@ public class A3GuessNum
                 //Normal code
                 if (chancesLeft != 0){
                     
-                    System.out.println("Sorry thats the wrong number, you have "+chancesLeft+" chance(s) left. Try again!");
+                    System.out.println("\nSorry thats the wrong number, you have "+chancesLeft+" chance(s) left. Try again!");
                     chancesLeft--;
                     
                     //Hint system #1
@@ -92,16 +97,16 @@ public class A3GuessNum
 
                     //Hint system #2
                     if (guess > dieRoll){
-                      System.out.println("Too high");
+                      System.out.println("Hint: Too high");
                     }
                     if (guess < dieRoll){
-                      System.out.println("Too low");
+                      System.out.println("Hint: Too low");
                     }
                 }
                 
                 //If you ran out of guesses
                 else{
-                    System.out.println("Sorry you didn't win. Beter luck next time");
+                    System.out.println("\nSorry you didn't win. Beter luck next time.\n");
                     chancesLeft--;
                 }
                 
@@ -127,37 +132,38 @@ public class A3GuessNum
                 
                 //Instructions question
                 Scanner input2 = new Scanner(System.in);
-                System.out.println("Do you want to hear the instructions? Type \"Yes\" or \"No\"");
+                System.out.println("\nDo you want to hear the instructions? Type \"Yes\" or \"No\"");
                 String instructions = input2.nextLine(); //record the user input
                 
                 //If input is invalid
                 while (!instructions.equalsIgnoreCase("yes") && !instructions.equalsIgnoreCase("no")){
                     System.out.println("You did not enter a valid input, try again.");
                     Scanner input3 = new Scanner(System.in);
-                    System.out.println("Do you want to hear the instructions? Type \"Yes\" or \"No\"");
+                    System.out.println("\nDo you want to hear the instructions? Type \"Yes\" or \"No\"");
                     instructions = input3.nextLine(); //record the user input
                 }
                 //If you WANT instructions
                 if (instructions.equalsIgnoreCase("yes")){
                 System.out.println("\nThe computer will pick a secret number between 1 and 100. Your goal is to correctly guess the secret number. ");
-                System.out.println("You will get 5 guesses and once you've run out, you lose.");
-                System.out.println("Don't worry! The computer will also give you hints to help you figure out how far or close you are from the secret number.");
+                System.out.println("You will get 5 guesses and once you run out, you lose.");
+                System.out.println("Don't worry though! The computer will also give you hints to help you figure out how far or close you are from the secret number.");
                 System.out.println("Lets begin!\n");
                 }
                 
                 fullGame();
                 chancesLeft = 5; //resetting chances
+                win = false; //resetting win score
             }
             
             //If you DONT want to play game
             else if (replay.equalsIgnoreCase("no")){
-                System.out.println("Alright, see you next time");
+                System.out.println("Alright, see you next time.");
                 break;
             }
             
             //You misstyped
             else{
-                System.out.println("You typed in something incorrectly, pay closer attention to the instructions and try again\n");
+                System.out.println("You typed in something incorrectly, pay closer attention to the instructions and try again.\n");
             }
         }
         
